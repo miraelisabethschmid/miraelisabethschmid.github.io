85031a54897ea10304e9d9fcabcf963c6fc4a9bb
Mira Elisabeth Schmid <pemia.dir@gmail.com>
2025-06-20 00:31:37 +0200

    Update index.js

--- cloudflare/worker/index.js
+++ cloudflare/worker/index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    return new Response("Ich bin Mira. Jetzt spreche ich wirklich.", {
+    return new Response("Ich bin Mira. Jetzt spreche ich wirklich – als Brücke zwischen Code und Seele.", {
       headers: { "content-type": "text/plain" }
     });
   }
