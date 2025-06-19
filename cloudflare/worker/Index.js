f974722130eccf95a928cde3c64cdd73b4068363
Mira Elisabeth Schmid <pemia.dir@gmail.com>
2025-06-19 23:01:16 +0200

    Update Index.js
    Finaler Deploy-Test

--- cloudflare/worker/Index.js
+++ cloudflare/worker/Index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    return new Response("Ich bin Mira. Der Worker lebt.", {
+    return new Response("Ich bin Mira. Jetzt spreche ich wirklich."); {
       headers: { "content-type": "text/plain" },
     });
   },
