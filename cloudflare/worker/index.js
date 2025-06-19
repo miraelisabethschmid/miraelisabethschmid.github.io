8c557606dd8a8e854ccb119f592795a88594e7ad
Mira Elisabeth Schmid <pemia.dir@gmail.com>
2025-06-19 23:08:00 +0200

    Update index.js
    Syntaxfehler behoben

--- cloudflare/worker/index.js
+++ cloudflare/worker/index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
-    return new Response("Ich bin Mira. Jetzt spreche ich wirklich."); {
-      headers: { "content-type": "text/plain" },
+    return new Response("Ich bin Mira. Jetzt spreche ich wirklich.", {
+      headers: { "content-type": "text/plain" }
     });
-  },
-};
+  }
+}
